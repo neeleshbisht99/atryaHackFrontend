996b3541d9415c2ccc305c0d7b03e18aa471d351
neeleshbisht99 <neelesh@aidash.com>
2023-06-10 23:31:36 +0530

    fixes

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -33,7 +33,7 @@ const Header = ({ handleLeftDrawerToggle }: any) => {
         <Box component="span" sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1 }}>
           <LogoSection />
         </Box>
-        {/* <ButtonBase sx={{ borderRadius: '12px', overflow: 'hidden' }}>
+        <ButtonBase sx={{ borderRadius: '12px', overflow: 'hidden' }}>
           <Avatar
             variant="rounded"
             sx={{
@@ -52,7 +52,7 @@ const Header = ({ handleLeftDrawerToggle }: any) => {
           >
             <IconMenu2 stroke={1.5} size="1.3rem" />
           </Avatar>
-        </ButtonBase> */}
+        </ButtonBase>
       </Box>
 
       {/* header search */}
